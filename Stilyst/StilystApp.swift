322a609f1a9e5dbc10187fc4c74094165fbcd889
Agajohn <rn_Esl@yahoo.com>
2025-10-21 08:39:49 -0700

    Initial Commit

---
+++ Stilyst/StilystApp.swift
@@ -0,0 +1,17 @@
+//
+//  StilystApp.swift
+//  Stilyst
+//
+//  Created by Aren Agajohn on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct StilystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StilystTests/StilystTests.swift
@@ -0,0 +1,17 @@
+//
+//  StilystTests.swift
+//  StilystTests
+//
+//  Created by Aren Agajohn on 10/21/25.
+//
+
+import Testing
+@testable import Stilyst
+
+struct StilystTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
